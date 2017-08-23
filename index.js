a820d112c634ee98fdf073db5d7535f965ec634b
Adiyam Nerri <adiyam@gmail.com>
2017-08-23 14:08:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function iterativeLog(array) {
     console.log(`${index}: ${element}`);
   })
 }
+
+var odds = [1,3,5,7,9]
+function iterate(callback) {
+  odds.forEach(callback => {
+    return odds;
+  })
+}
